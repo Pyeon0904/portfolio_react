00138d96c81ac612f16d51343c068447f596e029
LAPTOP-TVMKKI1G\admin <jjp2548@gmail.com>
2023-11-17 15:45:31 +0900

    profile_introduce

--- portfolio_react/src/pages/Project/Project.js
+++ portfolio_react/src/pages/Project/Project.js
@@ -9,8 +9,14 @@ export default function Project() {
         <div className="Title_line" />
       </div>
       <div class="row">
-        <div class="col">1 of 2</div>
-        <div class="col">2 of 2</div>
+        <div class="col">1 of 3</div>
+        <div class="col">2 of 3</div>
+        <div class="col">3 of 3</div>
+      </div>
+      <div class="row">
+        <div class="col">1 of 3</div>
+        <div class="col">2 of 3</div>
+        <div class="col">3 of 3</div>
       </div>
       <div class="row">
         <div class="col">1 of 3</div>
